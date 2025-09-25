5e9548fb787aa413ee903502bdc72c0995807eeb
playermiller109 <145541890+playermiller109@users.noreply.github.com>
2025-09-25 12:56:31 +0800

    Add files via upload

---
+++ src/mergeTable/handleFocus.js
@@ -0,0 +1,44 @@
+const { isSign } = require('../utils.js')
+const handleFocus = table=> {
+  const _old = table.receiveCellFocus
+  table.receiveCellFocus = function (row, col, func, flag) {
+    if (table.rows[row]?.[col]?.el.style.display == 'none') {
+      const { cell } = table.editor.tableCell
+      const cells = table.rows.flat()
+      const { row: maxRow, col: maxCol } = cells.pop()
+
+      if (row === cell.row) {
+        while (isSign(table.rows[row]?.[col]?.text)) {
+          col += (col < cell.col) ? -1 : 1
+        }
+
+        if (col < 0) {
+          while (isSign(table.rows[row]?.[0].text)) row--
+        }
+        if (col > maxCol) {
+          col = 0; row++
+          if (row > maxRow) table.insertRow(row, col)
+        }
+      }
+      else if (col === cell.col) {
+        while (isSign(table.rows[row]?.[col]?.text)) {
+          row += (row < cell.row) ? -1 : 1
+        }
+
+        if (row < 0) {
+          while (isSign(table.rows[0][col]?.text)) col--
+        }
+      }
+      else {
+        if (row === cell.row - 1) {
+          while (isSign(table.rows[row][col]?.text)) col--
+        }
+        if (row === cell.row + 1) {
+          while (isSign(table.rows[row][col]?.text)) col++
+        }
+      }
+    }
+    _old.call(this, row, col, func, flag)
+  }
+}
+module.exports = handleFocus
